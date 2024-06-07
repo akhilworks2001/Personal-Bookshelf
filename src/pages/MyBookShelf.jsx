0c51be47e54841e0584c1369e70311cfb017120b
akhil verma <akhil01verma@gmail.com>
2024-06-07 18:18:19 +0530

    Update MyBookShelf.jsx

--- src/pages/MyBookShelf.jsx
+++ src/pages/MyBookShelf.jsx
@@ -10,21 +10,10 @@ export default function MyBookShelf({ bookshelf, removeFromBookshelf }) {
           <BookCard 
             key={book.key}
             book={book}
-            removeFromBookshelf={removeFromBookshelf}
+            fun={removeFromBookshelf}
+            type={'remove'}
           />
-          // <div key={book.key} className="book-card">
-          //   <div className="container">
-          //     <h3>Book Title:</h3>
-          //     <p>{book?.title}</p>
-          //   </div>
-          //   <div className="container">
-          //     <h3>Book Edition:</h3>
-          //     <p>{book?.edition_count}</p>
-          //   </div>
-          //   <button onClick={() => removeFromBookshelf(book.key)}>
-          //     Remove
-          //   </button>
-          // </div>
+          
         ))}
       </div>
     </div>
